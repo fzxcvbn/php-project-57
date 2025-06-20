b7f0d045121efd70990c99f89797719ae3d2df84
fzxcvbn <diankahid@gmail.com>
2025-06-21 00:59:35 +0300

    update

--- database/seeders/TaskStatusSeeder.php
+++ database/seeders/TaskStatusSeeder.php
@@ -4,12 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\TaskStatus;
 
 class TaskStatusSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
         TaskStatus::firstOrCreate(['name' => 'новый']);
